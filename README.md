07813a9c37e98a1978ca44e9338ac40a9340f15e
ENVS <envs@eduroam-169-233-196-3.ucsc.edu>
2019-06-03 16:28:22 -0700

    functions for plotting PDF

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # beta_regression
-beta regression demo using glmmTMB
+Beta regression demo using glmmTMB
+
+Some code comes from Ben Bolker's "Getting started with the glmmTMB package" [vignette](https://cran.r-project.org/web/packages/glmmTMB/vignettes/glmmTMB.pdf).
+
--- beta_regression.R
+++ beta_regression.R
@@ -2,6 +2,8 @@ library(glmmTMB)
 library(bbmle)
 library(ggplot2)
 
+# For reparameterizing the beta distribution
+# "Classical" parameterization
 beta_ab2muphi <- function(a, b) {
   return(c(
     a/(a+b), 
@@ -9,6 +11,7 @@ beta_ab2muphi <- function(a, b) {
   ))
 }
 
+# Center and shape parameterization
 beta_muphi2ab <- function(mu, phi) {
   return(c(
     mu^2 * (1 - mu) / phi - mu,
@@ -16,3 +19,39 @@ beta_muphi2ab <- function(mu, phi) {
   ))
 }
 
+# Function for plotting beta pdf
+beta_pdf <- function(a, b, add=FALSE) {
+  grid <- seq(0,1, by=0.01)
+  if(add) {
+    points(x = grid,
+           y = dbeta(grid, a, b))
+  } else {
+    plot(x = grid,
+           y = dbeta(grid, a, b))
+  }
+}
+
+beta_pdf(1, 1, FALSE)
+beta_pdf(0.5, 0.5, TRUE)
+beta_pdf(0.9, 0.9, TRUE)
+beta_pdf(1, 0.9, TRUE)
+
+# Function for plotting beta pdf with center-shape parameterization
+beta_pdf2 <- function(mu, phi, add=FALSE) {
+  ab_parameters <- beta_muphi2ab(mu, phi)
+  a <- ab_parameters[1]
+  b <- ab_parameters[2]
+  grid <- seq(0,1, by=0.01)
+  if(add) {
+    points(x = grid,
+           y = dbeta(grid, a, b))
+  } else {
+    plot(x = grid,
+         y = dbeta(grid, a, b))
+  }
+}
+
+beta_pdf2(0.5, 0.25)
+
+
+# Randomly generate data with bimodal beta-distributed residuals
