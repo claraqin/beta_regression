edea3f493979a93be7d7509900da2bee38563268
ENVS <envs@eduroam-169-233-196-3.ucsc.edu>
2019-06-03 15:44:23 -0700

    reparameterization functions

--- beta_regression.R
+++ beta_regression.R
@@ -1,1 +1,18 @@
 library(glmmTMB)
+library(bbmle)
+library(ggplot2)
+
+beta_ab2muphi <- function(a, b) {
+  return(c(
+    a/(a+b), 
+    a*b/((a+b)^2 * (a+b+1))
+  ))
+}
+
+beta_muphi2ab <- function(mu, phi) {
+  return(c(
+    mu^2 * (1 - mu) / phi - mu,
+    mu / phi * (1 - mu)^2 + mu - 1
+  ))
+}
+
