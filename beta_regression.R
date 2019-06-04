7b8ac044790d047424ef3a3a5505ff2c56e3bc2b
ENVS <envs@eduroam-169-233-196-3.ucsc.edu>
2019-06-04 16:52:44 -0700

    revised to mean-and-dispersion parameters

--- beta_regression.R
+++ beta_regression.R
@@ -7,16 +7,16 @@ library(dplyr)
 # From "classical" parameterization
 beta_ab2muphi <- function(a, b) {
   return(c(
-    a/(a+b), 
-    a*b/((a+b)^2 * (a+b+1))
+    a/(a + b), 
+    a + b
   ))
 }
 
-# From center and shape parameterization
+# From mean-and-dispersion parameterization
 beta_muphi2ab <- function(mu, phi) {
   return(c(
-    mu^2 * (1 - mu) / phi - mu,
-    mu / phi * (1 - mu)^2 + mu - 1
+    mu * phi,
+    phi * (1 - mu)
   ))
 }
 
