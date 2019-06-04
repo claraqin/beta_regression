532557892bbcd30bb7c71863b4953e3d3ba6e89b
ENVS <envs@ENVS-Loaner-rMBP2.local>
2019-06-03 22:11:45 -0700

    simulated some data

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # beta_regression
 Beta regression demo using glmmTMB
 
-Some code comes from Ben Bolker's "Getting started with the glmmTMB package" [vignette](https://cran.r-project.org/web/packages/glmmTMB/vignettes/glmmTMB.pdf).
 
--- beta_regression.R
+++ beta_regression.R
@@ -1,9 +1,10 @@
 library(glmmTMB)
 library(bbmle)
 library(ggplot2)
+library(dplyr)
 
 # For reparameterizing the beta distribution
-# "Classical" parameterization
+# From "classical" parameterization
 beta_ab2muphi <- function(a, b) {
   return(c(
     a/(a+b), 
@@ -11,7 +12,7 @@ beta_ab2muphi <- function(a, b) {
   ))
 }
 
-# Center and shape parameterization
+# From center and shape parameterization
 beta_muphi2ab <- function(mu, phi) {
   return(c(
     mu^2 * (1 - mu) / phi - mu,
@@ -35,6 +36,7 @@ beta_pdf(1, 1, FALSE)
 beta_pdf(0.5, 0.5, TRUE)
 beta_pdf(0.9, 0.9, TRUE)
 beta_pdf(1, 0.9, TRUE)
+beta_pdf(5, 2)
 
 # Function for plotting beta pdf with center-shape parameterization
 beta_pdf2 <- function(mu, phi, add=FALSE) {
@@ -51,7 +53,36 @@ beta_pdf2 <- function(mu, phi, add=FALSE) {
   }
 }
 
-beta_pdf2(0.5, 0.25)
 
+# Randomly generate data with bistable beta-distributed residuals
+set.seed(1)
+x1 <- rnorm(250)
+x2 <- rnorm(250)
+beta1 <- -0.4
+beta2 <- 5
+y <- boot::inv.logit( beta1*x1 + beta2*x2 )
+hist(y)
+# This actually already looks like it has a 
+# bimodal beta error distribution, when in fact, 
+# it's beta-distributed *without* errors. 
 
-# Randomly generate data with bimodal beta-distributed residuals
+# Actually, inv.logit(rnorm(250, 0, x)) produces a
+# seemingly bimodal distribution bounded at 0 and 1
+# for any value of x < ~2.
+hist(boot::inv.logit(rnorm(250, 0, 1)))
+hist(boot::inv.logit(rnorm(250, 0, 2)))
+hist(boot::inv.logit(rnorm(250, 0, 3)))
+
+# Suppose we still need to add beta-distributed errors.
+# Since response values need to be bounded at 0 and 1, 
+# keep generating new beta-distributed errors,
+# adding them to the raw response if within bounds,
+# or rejecting them if exceeding the bounds. This
+# maintains the beta-distributed errors.
+epsilon_star <- rbeta(250, 0.5, 0.5)
+y_star <- y + epsilon_star - 0.5
+while(sum(y < 0 | y > 1)) { # untested
+  y <- ifelse(y_star < 0 | y_star > 1, y, y_star)
+  epsilon_star <- rbeta(250, 0.5, 0.5)
+  y_star <- y + epsilon_star
+}
